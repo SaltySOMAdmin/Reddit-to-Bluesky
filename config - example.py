c8067e34cbb2c1d432152d4ba6db83f6610f1e1a
TheMappyMapperson <157048676+TheMappyMapperson@users.noreply.github.com>
2025-02-06 15:35:03 -0500

    Updates
    Moved credentials and webhook to separate files. Consolidated private and public repositories. Updates Readme.

---
+++ config - example.py
@@ -0,0 +1,8 @@
+# rename to config.py, enter your credentials, and save to the same folder as your script
+
+REDDIT_CLIENT_ID = ""
+REDDIT_CLIENT_SECRET = ""
+REDDIT_USER_AGENT = ""
+
+BSKY_USERNAME = ""
+BSKY_PASSWORD = ""
