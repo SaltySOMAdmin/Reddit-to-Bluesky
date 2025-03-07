7ef3d6ab7505875fa8413dd3ff6ddd5e93e84c21
SaltySOMAdmin <157048676+SaltySOMAdmin@users.noreply.github.com>
2025-03-07 11:03:56 -0500

    secrets update

--- config - example.py
+++ config - example.py
@@ -1,4 +1,4 @@
-# rename to config.py, enter your credentials, and save to the same folder as your script
+# Rename this file to config.py. Enter your credentials and save to the same folder as your script. If you're using a public repo then ensure this file is in your .gitignore list! 
 
 REDDIT_CLIENT_ID = ""
 REDDIT_CLIENT_SECRET = ""
